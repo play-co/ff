56a78a532cbce159d7ed4f389652c2cba1ce67f9
Michael Henretty <michael.henretty@gmail.com>
2012-07-24 19:10:51 -0700

    adding ability to add context to ff, adding tests, bumping version

---
+++ test/index.js
@@ -0,0 +1,60 @@
+var assert = require("assert");
+var ff = require("../lib/ff");
+
+describe("ff", function () {
+
+	describe("#pass()", function () {
+		it("should pass a reference to the next step", function (done) {
+			ff(function () {
+				var foo = { bar: false };
+				this.pass(foo);
+				foo.bar = true;
+			}, function (foo) {
+				assert(foo);
+				assert(foo.bar);
+			}, ff.cb(done));
+		});
+	});
+
+	describe("#slot()", function () {
+		it("should pass values to the next function in slotted order", function (done) {
+			ff(function () {
+				var one = this.slot();
+				var two = this.slot();
+
+				setTimeout(function () {
+					one(null, 1);
+				}, 300);
+
+				setTimeout(function () {
+					two(null, 2);
+				}, 200)
+			}, function (one, two) {
+				assert.equal(one, 1);
+				assert.equal(two, 2);
+			}, ff.cb(done));
+		});
+	});
+
+	describe("#error()", function () {
+		it("should forward errors to the error callback", function (done) {
+			ff(function () {
+				throw "go straight to error";
+			}, function () {
+				assert.fail();
+			}, ff.error(function (err) {
+				assert.equal(err, "go straight to error");
+				done();
+			}));
+		});
+
+		it("should forward errors to the error callback", function (done) {
+			ff(function () {
+				// do nothing
+			}, ff.cb(done), ff.error(function (err) {
+				assert.fail();
+				done();
+			}));
+		});
+	});
+});
